102f4a285b8b24cc3ef0f42fd407249e385ce27c
Kishon <kishonmgmt@gmail.com>
2022-10-05 13:31:31 -0500

    SideMenuView UI changes

---
+++ Reveal/SearchViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewModel.swift
+//  Reveal
+//
+//  Created by kishon daniels on 7/27/22.
+//
+
+import SwiftUI
+
+struct SearchViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchViewModel()
+    }
+}
---
+++ Reveal/View/MainTab/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Reveal
+//
+//  Created by kishon daniels on 7/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
